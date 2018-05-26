ef73613e05de4213a59c7451041d9ab433118153
austinoboyle <14arob@queensu.ca>
2018-05-26 17:26:03 -0400

    Fix another silly typo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const MODEL_METHODS = require("methods");
 let mockModel = {};
 
 for (let method of MODEL_METHODS) {
-    mockedModel[method] = jest.fn();
+    mockModel[method] = jest.fn();
 }
 
 module.exports = mockModel;
