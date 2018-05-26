d1f9693044377c66b685a9c967bb507e0988aa5e
austinoboyle <14arob@queensu.ca>
2018-05-26 17:23:12 -0400

    Fix bug - change mockedJobs to mockModel in index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const MODEL_METHODS = require("methods");
 let mockModel = {};
 
 for (let method of MODEL_METHODS) {
-    mockedJobs[method] = jest.fn();
+    mockedModel[method] = jest.fn();
 }
 
 module.exports = mockModel;
